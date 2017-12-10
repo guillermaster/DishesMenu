f075ec281af1228e42988690afd4b2ec4131d276
Guillermo Pincay <guillermaster@gmail.com>
2017-12-10 17:29:38 -0500

    first commit dishes menu

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyCnhiSs4seMyw32bEgT7zVJZINzuKjaIcw",
+  authDomain: "dishes-menu.firebaseapp.com",
+  databaseURL: "https://dishes-menu.firebaseio.com",
+});
+
+export default base;
